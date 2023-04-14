f2bb3e49b3dbf8c18e661fb8a3e4629ffea2ca23
Jakub Henyk <s452627@labs.wmi.amu.edu.pl>
2023-04-14 13:09:04 +0200

    Added class schemat to system.py

--- system.py
+++ system.py
@@ -1,1 +1,29 @@
-#tu będzie system o imieniu 
+NAME = 'ABC'
+
+class NaturalLanguageUnderstanding():
+    def __init__(self, text):
+        self.text = text
+    def textToFrame():
+        pass
+
+class DialogueStateTracker():
+    def __init__(self, frame):
+        self.frame = frame
+    def NLUFrameToDSTFrame():
+        pass
+
+class DialoguePolicy():
+    def __init__(self, frame):
+        self.frame = frame
+    def DSTFrameToDPFrame():
+        pass
+
+class NaturalLanguageGeneration():
+    def __init__(self, frame):
+        self.frame = frame
+    def DPFrameToText():
+        pass
+
+if __name__ == "__main__":
+
+    print(f"Hello World. I am {NAME}")
